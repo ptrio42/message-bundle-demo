716abac9661a27dfaff6ef7313f4204e6d1afd4a
piotr42 <piotr_cz@aol.com>
2018-03-15 11:47:52 +0000

    Initial commit.

--- src/Ptrio/MessageBundle/Command/SendMessageCommand.php
+++ src/Ptrio/MessageBundle/Command/SendMessageCommand.php
@@ -3,7 +3,9 @@
 namespace App\Ptrio\MessageBundle\Command;
 
 use App\Ptrio\MessageBundle\Client\ClientInterface;
+use App\Ptrio\MessageBundle\Model\DeviceInterface;
 use App\Ptrio\MessageBundle\Model\DeviceManagerInterface;
+use App\Ptrio\MessageBundle\Model\MessageManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,13 +15,19 @@ class SendMessageCommand extends Command
 {
     private $client;
     private $deviceManager;
+    private $messageManager;
 
     protected static $defaultName = 'ptrio:message:send-message';
 
-    public function __construct(ClientInterface $client, DeviceManagerInterface $deviceManager)
+    public function __construct(
+        ClientInterface $client,
+        DeviceManagerInterface $deviceManager,
+        MessageManagerInterface $messageManager
+    )
     {
         $this->client = $client;
         $this->deviceManager = $deviceManager;
+        $this->messageManager = $messageManager;
 
         parent::__construct();
     }
@@ -29,21 +37,28 @@ class SendMessageCommand extends Command
         $this
             ->setDefinition([
             new InputArgument('body', InputArgument::REQUIRED),
-            new InputArgument('recipient', InputArgument::REQUIRED),
+            new InputArgument('device-names', InputArgument::IS_ARRAY),
         ]);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $messageBody = $input->getArgument('body');
-        $recipient = $input->getArgument('recipient');
+        $deviceNames = $input->getArgument('device-names');
 
-        if ($device = $this->deviceManager->findDeviceByName($recipient)) {
+        $devices = $this->deviceManager->findDevicesByNames($deviceNames);
+        foreach ($devices as $device) {
+            /** @var DeviceInterface $device */
+            $message = $this->messageManager->createMessage();
+            $message->setBody($messageBody);
+            $message->setDevice($device);
+            $message->setSentAt(new \DateTime('now'));
+            $this->messageManager->updateMessage($message);
+
             $response = $this->client->sendMessage($messageBody, $device->getToken());
 
+            $output->writeln('Message successfully sent do device `'.$device->getName().'`.');
             $output->writeln('Response: '.$response);
-        } else {
-            $output->writeln('No device found!');
         }
     }
 }
